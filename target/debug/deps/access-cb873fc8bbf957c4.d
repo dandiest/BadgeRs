5b907b3c62fec3e7e79348e3f4f9e41b82fc74c0
unknown <devivosamuel1@gmail.com>
2026-02-11 20:15:13 +0100

    Added .gitignore

---
+++ target/debug/deps/access-cb873fc8bbf957c4.d
@@ -0,0 +1,9 @@
+C:\Users\Samuel\Desktop\Developing\rust\access\target\debug\deps\access-cb873fc8bbf957c4.d: src\main.rs Cargo.toml
+
+C:\Users\Samuel\Desktop\Developing\rust\access\target\debug\deps\libaccess-cb873fc8bbf957c4.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
